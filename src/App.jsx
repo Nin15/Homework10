210893d046adee0bfb7349d70387347beb4ba638
Nino Shubitidze <132759949+Nin15@users.noreply.github.com>
2024-12-06 21:59:44 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 import Hobbies from "./pages/Hobbies";
 import Header from "./components/Header";
 import Projects from "./pages/Projects";
